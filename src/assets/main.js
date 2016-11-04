3c9db94e8af5c071daa1eba14abf520a72683e7a
Chuck Han <hanislovingit@gmail.com>
2016-11-04 17:15:45 -0500

    done.

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,4 +2,22 @@ $(function() {
 
   // your code will go here
 
+  $.ajax({
+    url: "https://www.codeschool.com/users/ChuckHan.json",
+    dataType: "jsonp",
+    success: function (response) {
+      console.log(response);
+
+      var innerHtml = "";
+
+      response.courses.completed.forEach(function (course) {
+        var divHtml = "<div class='course'><h3>" + course.title + "</h3><img src='" + course.badge + "'/><a href='" + course.url +
+                      "' target='_blank' class='btn btn-primary'>Go to course</a></div>";
+        innerHtml += divHtml;
+      });
+
+      $("#badges").html(innerHtml);
+    }
+  });
+
 });
